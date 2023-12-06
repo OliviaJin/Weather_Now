7d5fea05bba96c2680357437f7c46fb1cd33436a
Yunuuo <jiiny738@newschool.edu>
2023-12-06 17:25:06 -0500

    name

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.getElementById('search-button').addEventListener('click', function() {
-    const city = document.getElementById('search-box').value;
-    fetchWeatherData(city);
+    const locationName = document.getElementById('search-box').value;
+    fetchWeatherData(locationName);
 });
 
 function fetchWeatherData(city) {
-    const url = `https://weathernow-wigc.onrender.com/?city=${city}`;
+    const url = `https://weathernow-wigc.onrender.com/?locationName=${city}`;
 
     fetch(url)
     .then(response => response.json())
@@ -16,9 +16,8 @@ function fetchWeatherData(city) {
 
 function displayWeatherData(data) {
     const weatherDetails = document.getElementById('weather-details');
-    // Update this part based on the structure of the data you receive
     weatherDetails.innerHTML = `
-        <h3>${data.cityName}</h3>
+        <h3>${data.city}</h3>
         <p>Temperature: ${data.temperature}°C</p>
         <p>Humidity: ${data.humidity}%</p>
         <p>Wind Speed: ${data.windSpeed} m/s</p>
