bfeb97c5209e8e6fd8b9e1b83ac7071c2e47be48
Yunuuo <jiiny738@newschool.edu>
2023-12-11 08:12:22 -0500

    22

--- script.js
+++ script.js
@@ -1,34 +1,71 @@
-  document.addEventListener('DOMContentLoaded', function () {
-    const locationInput = document.getElementById('locationInput');
-    const searchButton = document.getElementById('search');
-    const weatherInfoContainer = document.getElementById('weatherInfoContainer');
+document.addEventListener("DOMContentLoaded", function () {
+  const searchButton = document.getElementById("searchButton");
+  const locationInput = document.getElementById("locationInput");
+  const weatherResultDiv = document.getElementById("weatherResult");
 
-    locationInput.addEventListener('keydown', function (event) {
-      if (event.key === 'Enter') {
-        searchWeather();
-      }
-    });
-
-    searchButton.addEventListener('click', searchWeather);
-
-    function searchWeather() {
-      const url = `https://weathernow-wigc.onrender.com/weather?locationName=${encodeURIComponent(locationInput.value)}`;
-
-      // Fetch data from the weather API
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          weatherInfoContainer.style.display = 'block';
-          weatherInfoContainer.innerHTML = `
-            <h2>${data.location}</h2>
-            <p>Temperature: ${data.temperature}</p>
-            <p>Humidity: ${data.humidity}</p>
-            <p>Wind Speed: ${data.windSpeed}</p>`;
-          // You can add more data points here as per the structure of your weather data
-        })
-        .catch(error => {
-          console.error('Error fetching weather data:', error);
-          weatherInfoContainer.innerHTML = 'Error fetching weather data. Please try again later.';
-        });
-    }
+  searchButton.addEventListener("click", function() {
+      searchWeather();
   });
+
+  async function searchWeather() {
+      const locationName = locationInput.value;
+      if (!locationName) {
+          weatherResultDiv.innerHTML = "<p>Please enter a location.</p>";
+          return;
+      }
+
+      try {
+          const response = await fetch(`https://weathernow-wigc.onrender.com/weather?locationName=${encodeURIComponent(locationName)}`);
+          if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          const weatherData = await response.json();
+          await displayWeatherData(weatherData);
+      } catch (error) {
+          console.error("Error fetching weather data:", error);
+          weatherResultDiv.innerHTML = `<p>Error fetching weather data.</p>`;
+      }
+  }
+
+  async function displayWeatherData(data) {
+      if (data && data.success && data.data) {
+          const weatherInfo = data.data;
+          const cityImageSrc = await getCityImageUrl(weatherInfo.name);
+
+          weatherResultDiv.innerHTML = `
+              <div class="weather-info">
+                  <div>
+                      <h2>Weather in ${weatherInfo.name}, ${weatherInfo.sys.country}</h2>
+                      <p>Temperature: ${weatherInfo.main.temp} K</p>
+                      <p>Feels Like: ${weatherInfo.main.feels_like} K</p>
+                      <p>Min Temperature: ${weatherInfo.main.temp_min} K</p>
+                      <p>Max Temperature: ${weatherInfo.main.temp_max} K</p>
+                      <p>Humidity: ${weatherInfo.main.humidity}%</p>
+                      <p>Wind Speed: ${weatherInfo.wind.speed} m/s</p>
+                      <p>Cloudiness: ${weatherInfo.clouds.all}%</p>
+                      <p>Weather: ${weatherInfo.weather[0].main} (${weatherInfo.weather[0].description})</p>
+                  </div>
+                  <div class="city-image">
+                      <img src="${cityImageSrc}" alt="${weatherInfo.name}">
+                  </div>
+              </div>
+          `;
+      } else {
+          weatherResultDiv.innerHTML = "<p>Weather data not available for this location.</p>";
+      }
+  }
+
+  async function getCityImageUrl(cityName) {
+      const accessKey = 'sws-5tCuhMq8ZvUpD031hGlBzaZpf-Lo75htof_Xduo';
+      const url = `https://api.unsplash.com/search/photos?query=${encodeURIComponent(cityName)}&client_id=${accessKey}`;
+
+      try {
+          const response = await fetch(url);
+          const data = await response.json();
+          return data.results[0]?.urls?.regular;
+      } catch (error) {
+          console.error("Error fetching city image:", error);
+          return 'background.jpg';
+      }
+  }
+});
